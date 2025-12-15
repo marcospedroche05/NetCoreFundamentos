172cbaa082500ddc5cb96ec2a23dbbdb4ea1becc
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-15 14:04:47 +0100

    Suma de numeros string

---
+++ NetCoreFundamentos/Form07SumarString.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form07SumarString
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtString = new TextBox();
+            lblResultado = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Introduce la cadena";
+            // 
+            // txtString
+            // 
+            txtString.Location = new Point(47, 78);
+            txtString.Name = "txtString";
+            txtString.Size = new Size(233, 23);
+            txtString.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(47, 160);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(62, 15);
+            lblResultado.TabIndex = 2;
+            lblResultado.Text = "Resultado:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 119);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Sumar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form07SumarString
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(324, 222);
+            Controls.Add(button1);
+            Controls.Add(lblResultado);
+            Controls.Add(txtString);
+            Controls.Add(label1);
+            Name = "Form07SumarString";
+            Text = "Form07SumarString";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtString;
+        private Label lblResultado;
+        private Button button1;
+    }
+}
---
+++ NetCoreFundamentos/Form07SumarString.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form07SumarString : Form
+    {
+        public Form07SumarString()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cadena = this.txtString.Text;
+            int resultado = 0;
+            for(int i = 0; i < cadena.Length; i++)
+            {
+                char caracter = cadena[i];
+                if (char.IsNumber(caracter))
+                {
+                    int num = (int)Char.GetNumericValue(caracter);
+                    resultado += num;
+                }
+            }
+            this.lblResultado.Text += " " + resultado;
+        }
+    }
+}
