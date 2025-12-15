4570cee51fe653d9dc0c43f9c5c9ea2057bcd0da
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-15 13:22:11 +0100

    Validar email hecho

---
+++ NetCoreFundamentos/FormPracticaValidarEmail.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class FormPracticaValidarEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtMail = new TextBox();
+            lblResultado = new Label();
+            btnValidar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Introduce una dirección de correo";
+            // 
+            // txtMail
+            // 
+            txtMail.Location = new Point(45, 71);
+            txtMail.Name = "txtMail";
+            txtMail.Size = new Size(366, 23);
+            txtMail.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(45, 145);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(22, 15);
+            lblResultado.TabIndex = 2;
+            lblResultado.Text = ".....";
+            // 
+            // btnValidar
+            // 
+            btnValidar.Location = new Point(45, 110);
+            btnValidar.Name = "btnValidar";
+            btnValidar.Size = new Size(132, 23);
+            btnValidar.TabIndex = 3;
+            btnValidar.Text = "Validar";
+            btnValidar.UseVisualStyleBackColor = true;
+            btnValidar.Click += btnValidar_Click;
+            // 
+            // FormPracticaValidarEmail
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(472, 245);
+            Controls.Add(btnValidar);
+            Controls.Add(lblResultado);
+            Controls.Add(txtMail);
+            Controls.Add(label1);
+            Name = "FormPracticaValidarEmail";
+            Text = "FormPracticaValidarEmail";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtMail;
+        private Label lblResultado;
+        private Button btnValidar;
+    }
+}
---
+++ NetCoreFundamentos/FormPracticaValidarEmail.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class FormPracticaValidarEmail : Form
+    {
+        public FormPracticaValidarEmail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            string email = this.txtMail.Text;
+            bool correcto = true;
+            string dominio = "";
+            int posicionPunto;
+            if (!email.Contains('.'))
+            {
+                MessageBox.Show("El correo debe contener al menos un .");
+                correcto = false;
+            }
+            else
+            {
+                posicionPunto = email.LastIndexOf('.');
+                dominio = email.Substring(posicionPunto + 1);
+            }
+
+            if (!email.Contains("@"))
+            {
+                MessageBox.Show("El correo debe contener @");
+                correcto = false;
+            } else if(email.StartsWith('@') || email.EndsWith('@'))
+            {
+                MessageBox.Show("El correo no debe contener un @ al inicio ni al final");
+                correcto = false;
+            } else if(email.Split('@').Length > 2)
+            {  
+                MessageBox.Show("El correo debe contener un solo @");
+                correcto = false;
+            } else if(email.IndexOf('.') < email.IndexOf('@'))
+            {
+                MessageBox.Show("El correo debe tener el . después del @");
+                correcto = false;
+            } else if(dominio.Length < 2 || dominio.Length > 3)
+            {
+                MessageBox.Show("El dominio debe tener una longitud de 2 o 3 caracteres");
+                correcto = false;
+            }
+
+            if (correcto)
+            {
+                this.lblResultado.Text = "Email correcto";
+            }
+
+        }
+    }
+}
