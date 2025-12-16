4589fd075ac3f506bfe030e6062894101b774187
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-16 11:41:20 +0100

    Form10 hecho

---
+++ NetCoreFundamentos/Form11ColeccionMultiple.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form11ColeccionMultiple
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblIndex = new Label();
+            lblItem = new Label();
+            btnBorrarTodo = new Button();
+            btnEliminar = new Button();
+            btnInsertar = new Button();
+            txtNuevoElemento = new TextBox();
+            label2 = new Label();
+            lstElementos = new ListBox();
+            label1 = new Label();
+            btnSeleccionados = new Button();
+            SuspendLayout();
+            // 
+            // lblIndex
+            // 
+            lblIndex.AutoSize = true;
+            lblIndex.Location = new Point(45, 361);
+            lblIndex.Name = "lblIndex";
+            lblIndex.Size = new Size(39, 15);
+            lblIndex.TabIndex = 17;
+            lblIndex.Text = "Indice";
+            // 
+            // lblItem
+            // 
+            lblItem.AutoSize = true;
+            lblItem.Location = new Point(45, 330);
+            lblItem.Name = "lblItem";
+            lblItem.Size = new Size(77, 15);
+            lblItem.TabIndex = 16;
+            lblItem.Text = "Seleccionado";
+            // 
+            // btnBorrarTodo
+            // 
+            btnBorrarTodo.Location = new Point(235, 204);
+            btnBorrarTodo.Name = "btnBorrarTodo";
+            btnBorrarTodo.Size = new Size(182, 23);
+            btnBorrarTodo.TabIndex = 15;
+            btnBorrarTodo.Text = "Borrar todo";
+            btnBorrarTodo.UseVisualStyleBackColor = true;
+            btnBorrarTodo.Click += btnBorrarTodo_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(235, 142);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(182, 36);
+            btnEliminar.TabIndex = 14;
+            btnEliminar.Text = "Eliminar seleccionados";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(235, 97);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(182, 23);
+            btnInsertar.TabIndex = 13;
+            btnInsertar.Text = "Insertar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += btnInsertar_Click;
+            // 
+            // txtNuevoElemento
+            // 
+            txtNuevoElemento.Location = new Point(235, 56);
+            txtNuevoElemento.Name = "txtNuevoElemento";
+            txtNuevoElemento.Size = new Size(182, 23);
+            txtNuevoElemento.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(235, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Nuevo elemento";
+            // 
+            // lstElementos
+            // 
+            lstElementos.FormattingEnabled = true;
+            lstElementos.Location = new Point(45, 56);
+            lstElementos.Name = "lstElementos";
+            lstElementos.Size = new Size(152, 259);
+            lstElementos.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(45, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Elementos";
+            // 
+            // btnSeleccionados
+            // 
+            btnSeleccionados.Location = new Point(235, 254);
+            btnSeleccionados.Name = "btnSeleccionados";
+            btnSeleccionados.Size = new Size(182, 37);
+            btnSeleccionados.TabIndex = 18;
+            btnSeleccionados.Text = "Seleccionados";
+            btnSeleccionados.UseVisualStyleBackColor = true;
+            btnSeleccionados.Click += btnSeleccionados_Click;
+            // 
+            // Form11ColeccionMultiple
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(465, 417);
+            Controls.Add(btnSeleccionados);
+            Controls.Add(lblIndex);
+            Controls.Add(lblItem);
+            Controls.Add(btnBorrarTodo);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnInsertar);
+            Controls.Add(txtNuevoElemento);
+            Controls.Add(label2);
+            Controls.Add(lstElementos);
+            Controls.Add(label1);
+            Name = "Form11ColeccionMultiple";
+            Text = "Form11ColeccionMultiple";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblIndex;
+        private Label lblItem;
+        private Button btnBorrarTodo;
+        private Button btnEliminar;
+        private Button btnInsertar;
+        private TextBox txtNuevoElemento;
+        private Label label2;
+        private ListBox lstElementos;
+        private Label label1;
+        private Button btnSeleccionados;
+    }
+}
---
+++ NetCoreFundamentos/Form11ColeccionMultiple.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form11ColeccionMultiple : Form
+    {
+        public Form11ColeccionMultiple()
+        {
+            InitializeComponent();
+            //LA LISTA TENDRA SELECCION MULTIPLE
+            this.lstElementos.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            string elem = this.txtNuevoElemento.Text;
+            this.lstElementos.Items.Add(elem);
+            this.txtNuevoElemento.Focus();
+            this.txtNuevoElemento.SelectAll();
+        }
+
+        private void btnSeleccionados_Click(object sender, EventArgs e)
+        {
+            string indices = "";
+            string items = "";
+            //VAMOS A RECORRER LA COLECCION DE ITEMS SELECCIONADOS
+            foreach (string item in this.lstElementos.SelectedItems)
+            {
+                items += item + ",";
+            }
+            foreach (int index in this.lstElementos.SelectedIndices)
+            {
+                indices += index + ",";
+            }
+            this.lblIndex.Text = indices.Trim(',');
+            this.lblItem.Text = items.Trim(',');
+        }
+
+        private void btnBorrarTodo_Click(object sender, EventArgs e)
+        {
+            this.lstElementos.Items.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //QUIERO ELIMINAR LOS ELEMENTOS SELCCIONADOS
+            //for(int i = 0; i < this.lstElementos.SelectedIndices.Count; i++)
+            //{
+            //    int index = this.lstElementos.SelectedIndices[i];
+            //    //ELIMINAMOS DE LA COLECCION
+            //    this.lstElementos.Items.RemoveAt(index);
+            //}
+            int numElementos = this.lstElementos.SelectedIndices.Count - 1;
+            for(int i = numElementos; i >= 0; i--)
+            {
+                int index = this.lstElementos.SelectedIndices[i];
+                //ELIMINAMOS DE LA COLECCION
+                this.lstElementos.Items.RemoveAt(index);
+            }
+        }
+    }
+}
