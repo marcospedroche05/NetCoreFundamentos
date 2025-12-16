24729ee7b63afc34c967fef50d60cf84d0edfd79
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-16 11:05:32 +0100

    fORM 10 HECHO

---
+++ NetCoreFundamentos/Form10ColeccionGrafica.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form10ColeccionGrafica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstElementos = new ListBox();
+            label2 = new Label();
+            txtNuevoElemento = new TextBox();
+            btnInsertar = new Button();
+            btnEliminar = new Button();
+            btnBorrarTodo = new Button();
+            lblItem = new Label();
+            lblIndex = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(33, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Elementos";
+            // 
+            // lstElementos
+            // 
+            lstElementos.FormattingEnabled = true;
+            lstElementos.Location = new Point(33, 69);
+            lstElementos.Name = "lstElementos";
+            lstElementos.Size = new Size(152, 259);
+            lstElementos.TabIndex = 1;
+            lstElementos.SelectedIndexChanged += lstElementos_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(223, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(95, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Nuevo elemento";
+            // 
+            // txtNuevoElemento
+            // 
+            txtNuevoElemento.Location = new Point(223, 69);
+            txtNuevoElemento.Name = "txtNuevoElemento";
+            txtNuevoElemento.Size = new Size(182, 23);
+            txtNuevoElemento.TabIndex = 3;
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(223, 110);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(182, 23);
+            btnInsertar.TabIndex = 4;
+            btnInsertar.Text = "Insertar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += btnInsertar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(223, 155);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(182, 23);
+            btnEliminar.TabIndex = 5;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnBorrarTodo
+            // 
+            btnBorrarTodo.Location = new Point(223, 217);
+            btnBorrarTodo.Name = "btnBorrarTodo";
+            btnBorrarTodo.Size = new Size(182, 23);
+            btnBorrarTodo.TabIndex = 6;
+            btnBorrarTodo.Text = "Borrar todo";
+            btnBorrarTodo.UseVisualStyleBackColor = true;
+            btnBorrarTodo.Click += btnBorrarTodo_Click;
+            // 
+            // lblItem
+            // 
+            lblItem.AutoSize = true;
+            lblItem.Location = new Point(33, 343);
+            lblItem.Name = "lblItem";
+            lblItem.Size = new Size(77, 15);
+            lblItem.TabIndex = 7;
+            lblItem.Text = "Seleccionado";
+            // 
+            // lblIndex
+            // 
+            lblIndex.AutoSize = true;
+            lblIndex.Location = new Point(33, 374);
+            lblIndex.Name = "lblIndex";
+            lblIndex.Size = new Size(39, 15);
+            lblIndex.TabIndex = 8;
+            lblIndex.Text = "Indice";
+            // 
+            // Form10ColeccionGrafica
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(439, 450);
+            Controls.Add(lblIndex);
+            Controls.Add(lblItem);
+            Controls.Add(btnBorrarTodo);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnInsertar);
+            Controls.Add(txtNuevoElemento);
+            Controls.Add(label2);
+            Controls.Add(lstElementos);
+            Controls.Add(label1);
+            Name = "Form10ColeccionGrafica";
+            Text = "Form10ColeccionGrafica";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstElementos;
+        private Label label2;
+        private TextBox txtNuevoElemento;
+        private Button btnInsertar;
+        private Button btnEliminar;
+        private Button btnBorrarTodo;
+        private Label lblItem;
+        private Label lblIndex;
+    }
+}
