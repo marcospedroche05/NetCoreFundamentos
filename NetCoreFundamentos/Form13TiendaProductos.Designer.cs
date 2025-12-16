3e18f7534d2c7ead884f940589ffe964781fab8c
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-16 14:01:58 +0100

    Form 13 hecho

---
+++ NetCoreFundamentos/Form13TiendaProductos.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form13TiendaProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtProducto = new TextBox();
+            btnNuevo = new Button();
+            btnEliminar = new Button();
+            btnBorrarTodo = new Button();
+            label2 = new Label();
+            lstTienda = new ListBox();
+            btnSeleccion = new Button();
+            btnTodos = new Button();
+            label3 = new Label();
+            lstAlmacen = new ListBox();
+            btnSubir = new Button();
+            btnBajar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Producto";
+            // 
+            // txtProducto
+            // 
+            txtProducto.Location = new Point(31, 50);
+            txtProducto.Name = "txtProducto";
+            txtProducto.Size = new Size(117, 23);
+            txtProducto.TabIndex = 1;
+            // 
+            // btnNuevo
+            // 
+            btnNuevo.Location = new Point(31, 92);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(117, 23);
+            btnNuevo.TabIndex = 2;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(31, 135);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(117, 23);
+            btnEliminar.TabIndex = 3;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnBorrarTodo
+            // 
+            btnBorrarTodo.Location = new Point(31, 176);
+            btnBorrarTodo.Name = "btnBorrarTodo";
+            btnBorrarTodo.Size = new Size(117, 27);
+            btnBorrarTodo.TabIndex = 4;
+            btnBorrarTodo.Text = "Borrar todo";
+            btnBorrarTodo.UseVisualStyleBackColor = true;
+            btnBorrarTodo.Click += btnBorrarTodo_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(216, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Tienda";
+            // 
+            // lstTienda
+            // 
+            lstTienda.FormattingEnabled = true;
+            lstTienda.Location = new Point(216, 49);
+            lstTienda.Name = "lstTienda";
+            lstTienda.Size = new Size(120, 154);
+            lstTienda.TabIndex = 6;
+            // 
+            // btnSeleccion
+            // 
+            btnSeleccion.Location = new Point(342, 66);
+            btnSeleccion.Name = "btnSeleccion";
+            btnSeleccion.Size = new Size(75, 23);
+            btnSeleccion.TabIndex = 7;
+            btnSeleccion.Text = "Seleccion";
+            btnSeleccion.UseVisualStyleBackColor = true;
+            btnSeleccion.Click += btnSeleccion_Click;
+            // 
+            // btnTodos
+            // 
+            btnTodos.Location = new Point(342, 110);
+            btnTodos.Name = "btnTodos";
+            btnTodos.Size = new Size(75, 23);
+            btnTodos.TabIndex = 8;
+            btnTodos.Text = "Todos";
+            btnTodos.UseVisualStyleBackColor = true;
+            btnTodos.Click += btnTodos_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(463, 32);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Almacén";
+            // 
+            // lstAlmacen
+            // 
+            lstAlmacen.FormattingEnabled = true;
+            lstAlmacen.Location = new Point(463, 50);
+            lstAlmacen.Name = "lstAlmacen";
+            lstAlmacen.Size = new Size(120, 154);
+            lstAlmacen.TabIndex = 10;
+            // 
+            // btnSubir
+            // 
+            btnSubir.Location = new Point(602, 66);
+            btnSubir.Name = "btnSubir";
+            btnSubir.Size = new Size(75, 23);
+            btnSubir.TabIndex = 11;
+            btnSubir.Text = "Subir";
+            btnSubir.UseVisualStyleBackColor = true;
+            btnSubir.Click += btnSubir_Click;
+            // 
+            // btnBajar
+            // 
+            btnBajar.Location = new Point(602, 110);
+            btnBajar.Name = "btnBajar";
+            btnBajar.Size = new Size(75, 23);
+            btnBajar.TabIndex = 12;
+            btnBajar.Text = "Bajar";
+            btnBajar.UseVisualStyleBackColor = true;
+            btnBajar.Click += btnBajar_Click;
+            // 
+            // Form13TiendaProductos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(709, 257);
+            Controls.Add(btnBajar);
+            Controls.Add(btnSubir);
+            Controls.Add(lstAlmacen);
+            Controls.Add(label3);
+            Controls.Add(btnTodos);
+            Controls.Add(btnSeleccion);
+            Controls.Add(lstTienda);
+            Controls.Add(label2);
+            Controls.Add(btnBorrarTodo);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnNuevo);
+            Controls.Add(txtProducto);
+            Controls.Add(label1);
+            Name = "Form13TiendaProductos";
+            Text = "Form13TiendaProductos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtProducto;
+        private Button btnNuevo;
+        private Button btnEliminar;
+        private Button btnBorrarTodo;
+        private Label label2;
+        private ListBox lstTienda;
+        private Button btnSeleccion;
+        private Button btnTodos;
+        private Label label3;
+        private ListBox lstAlmacen;
+        private Button btnSubir;
+        private Button btnBajar;
+    }
+}
---
+++ NetCoreFundamentos/Form13TiendaProductos.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form13TiendaProductos : Form
+    {
+        public Form13TiendaProductos()
+        {
+            InitializeComponent();
+            this.lstTienda.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            string producto = this.txtProducto.Text;
+            this.lstTienda.Items.Add(producto);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            for (int i = this.lstTienda.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                int indice = this.lstTienda.SelectedIndices[i];
+                this.lstTienda.Items.RemoveAt(indice);
+            }
+        }
+
+        private void btnBorrarTodo_Click(object sender, EventArgs e)
+        {
+            this.lstTienda.Items.Clear();
+        }
+
+        private void btnSeleccion_Click(object sender, EventArgs e)
+        {
+            for (int i = this.lstTienda.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                int indice = this.lstTienda.SelectedIndices[i];
+                object itemTienda = this.lstTienda.Items[indice];
+                this.lstAlmacen.Items.Add(itemTienda);
+                this.lstTienda.Items.RemoveAt(indice);
+            }
+        }
+
+        private void btnTodos_Click(object sender, EventArgs e)
+        {
+            foreach (string elemento in this.lstTienda.Items)
+            {
+                this.lstAlmacen.Items.Add(elemento);
+            }
+            this.lstTienda.Items.Clear();
+        }
+
+        private void btnSubir_Click(object sender, EventArgs e)
+        {
+            int indexSeleccionado = this.lstAlmacen.SelectedIndex;
+            string elementoSeleccionado = this.lstAlmacen.SelectedItem.ToString();
+            int indiceAnterior = indexSeleccionado - 1;
+
+            if (indiceAnterior >= 0)
+            {
+                string auxElementoAnterior = this.lstAlmacen.Items[indiceAnterior].ToString();
+                this.lstAlmacen.Items[indiceAnterior] = elementoSeleccionado;
+                this.lstAlmacen.Items[indexSeleccionado] = auxElementoAnterior;
+                this.lstAlmacen.SelectedIndex = indiceAnterior;
+
+                if(this.lstAlmacen.SelectedIndex == 0)
+                {
+                    this.btnSubir.Enabled = false;
+                }
+                this.btnBajar.Enabled = true;
+            }
+        }
+
+        private void btnBajar_Click(object sender, EventArgs e)
+        {
+            int indexSeleccionado = this.lstAlmacen.SelectedIndex;
+            string elementoSeleccionado = this.lstAlmacen.SelectedItem.ToString();
+            int indiceSiguiente = indexSeleccionado + 1;
+
+            if (indiceSiguiente <= this.lstAlmacen.Items.Count - 1)
+            {
+                string auxElementoSiguiente = this.lstAlmacen.Items[indiceSiguiente].ToString();
+                this.lstAlmacen.Items[indiceSiguiente] = elementoSeleccionado;
+                this.lstAlmacen.Items[indexSeleccionado] = auxElementoSiguiente;
+                this.lstAlmacen.SelectedIndex = indiceSiguiente;
+                if(this.lstAlmacen.SelectedIndex == this.lstAlmacen.Items.Count - 1)
+                {
+                    this.btnBajar.Enabled = false;
+                }
+                this.btnSubir.Enabled = true;
+            }
+        }
+    }
+}
