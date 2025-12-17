e0429c2a168bd08f16d2274d39178f55cedf1217
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-17 11:22:11 +0100

    Form15 hecho

---
+++ NetCoreFundamentos/Form15ArrayList.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form15ArrayList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(44, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(126, 39);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(44, 109);
+            button2.Name = "button2";
+            button2.Size = new Size(126, 39);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(44, 172);
+            button3.Name = "button3";
+            button3.Size = new Size(126, 39);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 249);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(192, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // Form15ArrayList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(216, 296);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form15ArrayList";
+            Text = "Form15ArrayList";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+    }
+}
---
+++ NetCoreFundamentos/Form15ArrayList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form15ArrayList : Form
+    {
+        public Form15ArrayList()
+        {
+            InitializeComponent();
+            //VAMOS A CREAR UNA COLECCION NO TIPADA
+            ArrayList coleccion = new ArrayList();
+            coleccion.Add(this.button1);
+            coleccion.Add(this.button2);
+            coleccion.Add(this.button3);
+            //SI DESEAMOS ACCEDER A UN OBJETO Y MODIFICAR ALGUNA CARACTERISTICA
+            //NECESITAMOS REALIZAR CASTING
+            ((Button)coleccion[0]).BackColor = Color.Yellow;
+            //PODEMOS REALIZAR CASTING CON FOR EACH
+            foreach (Button boton in coleccion)
+            {
+                boton.BackColor = Color.LightCoral;
+            }
+            //Y SI AÑADIMOS OTRO CONTROL QUE NO SEA UN BUTTON???
+            coleccion.Add(this.textBox1);
+            //NOS ABSTRAEMOS PARA CASTING EN TODOS LOS OBJETOS
+            foreach (Control boton in coleccion)
+            {
+                boton.BackColor = Color.LightCoral;
+                //CUANDO TENEMOS MULTIPLES OBJETOS DE DIFERENTES CLASES,
+                //NOS PODEMOS ABSTRAER EN EL RECORRIDO, PERO A LO MEJOR 
+                //NECESITAMOS ALGUNA CARACTERISTICA ESPECIAL DE CADA UNO.
+                //UNA CAJA TIENE UN METODO LLAMADO Paste()
+                if(boton is TextBox)
+                {
+                    ((TextBox)boton).Paste();
+                }
+            }
+
+        }
+    }
+}
