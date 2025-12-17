b4e13e4b30d593bfc8441d88b9aff1677f7d44d8
marcospedroche05 <marcospedroche62@gmail.com>
2025-12-17 09:51:43 +0100

    Form 14 hecho

---
+++ NetCoreFundamentos/Form14Metodos.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form14Metodos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNumero = new TextBox();
+            lblResultado = new Label();
+            btnValor = new Button();
+            btnDobleReferencia = new Button();
+            btnReferencia = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(49, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Número";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(49, 67);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 23);
+            txtNumero.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(49, 123);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 15);
+            lblResultado.TabIndex = 2;
+            // 
+            // btnValor
+            // 
+            btnValor.Location = new Point(217, 47);
+            btnValor.Name = "btnValor";
+            btnValor.Size = new Size(117, 61);
+            btnValor.TabIndex = 3;
+            btnValor.Text = "Doble Valor";
+            btnValor.UseVisualStyleBackColor = true;
+            btnValor.Click += btnValor_Click;
+            // 
+            // btnDobleReferencia
+            // 
+            btnDobleReferencia.Location = new Point(217, 123);
+            btnDobleReferencia.Name = "btnDobleReferencia";
+            btnDobleReferencia.Size = new Size(117, 61);
+            btnDobleReferencia.TabIndex = 4;
+            btnDobleReferencia.Text = "Doble referencia";
+            btnDobleReferencia.UseVisualStyleBackColor = true;
+            btnDobleReferencia.Click += btnDobleReferencia_Click;
+            // 
+            // btnReferencia
+            // 
+            btnReferencia.Location = new Point(217, 201);
+            btnReferencia.Name = "btnReferencia";
+            btnReferencia.Size = new Size(117, 61);
+            btnReferencia.TabIndex = 5;
+            btnReferencia.Text = "Objeto referencia";
+            btnReferencia.UseVisualStyleBackColor = true;
+            btnReferencia.Click += btnReferencia_Click;
+            // 
+            // Form14Metodos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(390, 310);
+            Controls.Add(btnReferencia);
+            Controls.Add(btnDobleReferencia);
+            Controls.Add(btnValor);
+            Controls.Add(lblResultado);
+            Controls.Add(txtNumero);
+            Controls.Add(label1);
+            Name = "Form14Metodos";
+            Text = "Form14Metodos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumero;
+        private Label lblResultado;
+        private Button btnValor;
+        private Button btnDobleReferencia;
+        private Button btnReferencia;
+    }
+}
---
+++ NetCoreFundamentos/Form14Metodos.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form14Metodos : Form
+    {
+        public Form14Metodos()
+        {
+            InitializeComponent();
+        }
+
+        //RECIBIMOS UN WRAPPER POR VALOR
+        void GetDobleValor(int numero)
+        {
+            numero *= 2;
+        }
+        void CambiarColor(Button boton)
+        {
+            boton.BackColor = Color.Honeydew;
+        }
+        void GetDobleReferencia(ref int numero)
+        {
+            numero *= 2;
+        }
+
+        private void btnValor_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(this.txtNumero.Text);
+            this.GetDobleValor(num);
+            this.lblResultado.Text = num.ToString();
+        }
+
+        private void btnReferencia_Click(object sender, EventArgs e)
+        {
+            this.CambiarColor(this.btnReferencia);
+        }
+
+        private void btnDobleReferencia_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(this.txtNumero.Text);
+            //CON REF, LAS DOS VARIABLES APUNTAN AL MISMO ESPACIO DE MEMORIA
+            this.GetDobleReferencia(ref num);
+            this.lblResultado.Text = num.ToString();
+        }
+    }
+}
