3886b1c076bf704ae906f9b1dcaad538f29d45d3
marcospedroche05 <marcospedroche62@gmail.com>
2026-01-09 11:32:43 +0100

    Leer y escribir mascotas

---
+++ NetCoreFundamentos/Form22MascotasFile.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form22MascotasFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNombre = new TextBox();
+            label2 = new Label();
+            txtRaza = new TextBox();
+            btnNuevaMascota = new Button();
+            btnRead = new Button();
+            btnWrite = new Button();
+            label3 = new Label();
+            lstMascotas = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(44, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(44, 68);
+            txtNombre.Multiline = true;
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(165, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(44, 127);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Raza";
+            // 
+            // txtRaza
+            // 
+            txtRaza.Location = new Point(44, 164);
+            txtRaza.Name = "txtRaza";
+            txtRaza.Size = new Size(165, 23);
+            txtRaza.TabIndex = 3;
+            // 
+            // btnNuevaMascota
+            // 
+            btnNuevaMascota.Location = new Point(44, 218);
+            btnNuevaMascota.Name = "btnNuevaMascota";
+            btnNuevaMascota.Size = new Size(165, 23);
+            btnNuevaMascota.TabIndex = 4;
+            btnNuevaMascota.Text = "Nueva mascota";
+            btnNuevaMascota.UseVisualStyleBackColor = true;
+            btnNuevaMascota.Click += btnNuevaMascota_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(44, 269);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(165, 23);
+            btnRead.TabIndex = 5;
+            btnRead.Text = "Read file";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // btnWrite
+            // 
+            btnWrite.Location = new Point(44, 318);
+            btnWrite.Name = "btnWrite";
+            btnWrite.Size = new Size(165, 23);
+            btnWrite.TabIndex = 6;
+            btnWrite.Text = "Write File";
+            btnWrite.UseVisualStyleBackColor = true;
+            btnWrite.Click += btnWrite_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(285, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Mascotas";
+            // 
+            // lstMascotas
+            // 
+            lstMascotas.FormattingEnabled = true;
+            lstMascotas.Location = new Point(285, 68);
+            lstMascotas.Name = "lstMascotas";
+            lstMascotas.Size = new Size(188, 274);
+            lstMascotas.TabIndex = 8;
+            // 
+            // Form22MascotasFile
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(533, 417);
+            Controls.Add(lstMascotas);
+            Controls.Add(label3);
+            Controls.Add(btnWrite);
+            Controls.Add(btnRead);
+            Controls.Add(btnNuevaMascota);
+            Controls.Add(txtRaza);
+            Controls.Add(label2);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Name = "Form22MascotasFile";
+            Text = "Form22MascotasFile";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNombre;
+        private Label label2;
+        private TextBox txtRaza;
+        private Button btnNuevaMascota;
+        private Button btnRead;
+        private Button btnWrite;
+        private Label label3;
+        private ListBox lstMascotas;
+    }
+}
---
+++ ProyectoClases/Helpers/HelperMascotas.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using ProyectoClases.Models;
+
+namespace ProyectoClases.Helpers
+{
+    public class HelperMascotas
+    {
+        public List<Mascota> Mascotas { get; set; }
+        //QUEREMOS ALMACENAR DE FORMA FISICA LAS MASCOTAS EN FILES
+        private HelperFiles helper;
+        private string path;
+
+        public HelperMascotas()
+        {
+            this.Mascotas = new List<Mascota>();
+            this.helper = new HelperFiles();
+            this.path = "mascotas.txt";
+        }
+
+        //CONVERTIR EL STRING DEL READ A LIST
+        private void ConvertirMascotasList(string data)
+        {
+            //GARFIELD,GATO@PLUTO,PERRO
+            this.Mascotas.Clear();
+            //SEPARAMOS CADA MASCOTA CON @
+            string[] datosMascota = data.Split('@');
+            foreach (string stringmascota in datosMascota)
+            {
+                string[] propiedades = stringmascota.Split(',');
+                Mascota mascota = new Mascota(propiedades[0], propiedades[1]);
+                this.Mascotas.Add(mascota);
+            }
+        }
+        //EL PROGRAMADOR ESCRIBA READ Y LEEMOS UN STRING Y RELLENAMOS LA COLECCIÓN
+        public async Task ReadMascotasAsync()
+        {
+            //LEEMOS EL FICHERO DE MASCOTAS
+            string data = await this.helper.ReadFileAsync(this.path);
+            //CONVERTIMOS EL STRING EN LIST
+            this.ConvertirMascotasList(data);
+        }
+
+        //CONVERTIR LA COLECCION A STRING
+        private string ConvertirMascotasString()
+        {
+            string data = "";
+            foreach (Mascota mascota in this.Mascotas){
+                string propiedades = mascota.Nombre + "," + mascota.Raza;
+                data += propiedades + "@";
+            }
+            data = data.TrimEnd('@');
+            return data;
+        }
+
+        //EL PROGRAMADOR ESCRIBA WRITE Y GUARDAMOS LAS MASCOTAS
+
+        public async Task WriteMascotasAsync()
+        {
+            //CONVERTIMOS LA COLECCION A STRING
+            string data = this.ConvertirMascotasString();
+            await this.helper.WriteFileAsync(this.path, data);
+        }
+
+    }
+}
